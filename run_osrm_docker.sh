2950ec0fb58dec16c2a8062574b560907759d45c
Zane Blanton <zeblanton@gmail.com>
2016-04-03 19:54:59 -0500

    added a second volume so that I had access to the binaries

--- run_osrm_docker.sh
+++ run_osrm_docker.sh
@@ -4,6 +4,9 @@ pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd -P`
 popd > /dev/null
 
-docker run -p 5000:5000 -v $SCRIPTPATH/../osrm/osrm-chi-new/:/this_is_data -it --name pavement_osrm pavement/osrm /bin/bash
+#docker run -p 5000:5000 -v $SCRIPTPATH/../osrm/osrm-chi-new/:/this_is_data -it --name pavement_osrm pavement/osrm /bin/bash
 
-#docker create -p 5000:5000 -v /home/zblan/Dropbox/project-pavement/osrm/osrm-chi-endpoints/:/data pavement/osrm --name pavement_osrm
+docker run -it -p 5000:5000 \
+	-v /home/zblan/Dropbox/project-pavement/osrm/osrm-chi-endpoints/:/data \
+	-v /home/zblan/Dropbox/project-pavement/osrm-backend-endpoints:/build \
+	--name pavement_osrm pavement/osrm /bin/bash
